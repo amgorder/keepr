eafd82213eda434b8589386191b419a650354778
Aaron Gorder <aarongorder@gmail.com>
2021-04-19 16:48:21 -0600

    Keep Pic CSS

--- keepr-client/src/components/KeepComponent.vue
+++ keepr-client/src/components/KeepComponent.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="keepComponent col-3">
-    <!-- not getting my keep info -->
-    <h3>{{ keepProp }}</h3>
-    HeLLo WorLd
+  <div class="keepComponent text-center col-3 card m-2">
+    <img class="card-img-center mt-3 pic d-flex justify-content-center" :src="keepProp.img" alt="">
+    <h3>{{ keepProp.name }}</h3>
   </div>
 </template>
 
@@ -25,4 +24,21 @@ export default {
 </script>
 
 <style scoped>
+
+.card {
+  height: 20em;
+  width: 10em;
+}
+.pic{
+  display: inline-block;
+  padding: 8px;
+
+  background: linear-gradient(130deg, rgb(187, 9, 241), rgb(6, 228, 236));
+  object-fit: cover;
+  margin: auto;
+  border-radius: 50%;
+  width: 200px;
+  height:200px;
+  box-shadow: 6px,6px,12px,16px black;
+}
 </style>
