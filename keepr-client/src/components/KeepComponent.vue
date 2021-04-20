29fcb566741617e16a933e978d74268803cfda9b
Aaron Gorder <aarongorder@gmail.com>
2021-04-19 22:01:57 -0600

    Keep Modal Added

--- keepr-client/src/components/KeepComponent.vue
+++ keepr-client/src/components/KeepComponent.vue
@@ -1,9 +1,45 @@
 <template>
   <div class="grid">
-    <div class="keepComponent card grid-item grid-item--width2">
+    <div class="keepComponent card bg-img grid-item grid-item--width2">
       <img class="card" :src="keepProp.img" alt="">
       <h3>{{ keepProp.name }}<img class="m-3 justify-content-right pic" :src="keepProp.creator.picture" alt=""></h3>
-      <p>{{ keepProp.description }}</p>
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+        Keep Info Modal
+      </button>
+    </div>
+  </div>
+
+  <!-- Modal -->
+  <div class="modal fade"
+       id="exampleModal"
+       tabindex="-1"
+       role="dialog"
+       aria-labelledby="exampleModalLabel"
+       aria-hidden="true"
+  >
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">
+            {{ keepProp.name }}
+          </h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <img class="card" :src="keepProp.img" alt="">
+
+          <p>{{ keepProp.description }}</p>
+          <img class="m-3 justify-content-right pic" :src="keepProp.creator.picture" alt="">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">
+            Close
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -36,6 +72,11 @@ export default {
   padding: 3px;
   margin: 3px;
 }
+
+/* .bg-img{
+  background-image: url("keepProp.img");
+} */
+
 .pic{
   display: inline-block;
   padding: 4px;
