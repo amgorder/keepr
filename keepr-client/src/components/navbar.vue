5cee58d0b8baf99eaf209c5211a52226d03fb268
Aaron Gorder <aarongorder@gmail.com>
2021-04-27 16:50:04 -0600

    Logo placement

--- keepr-client/src/components/navbar.vue
+++ keepr-client/src/components/navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg toggle">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
-        <!-- <img
+        <h1>Kr</h1><!-- <img
           alt="logo"
           src="../assets/img/cw-logo.png"
           height="45"
